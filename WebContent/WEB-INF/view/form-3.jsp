9424fc4f6209091ca8429afe7b006dd858a5b5ac
mandy1339 <mandy1339@gmail.com>
2018-07-16 20:07:57 -0400

    automated git push script

---
+++ WebContent/WEB-INF/view/form-3.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><title>Toledo FORM 3</title></head>
+<body>
+	<h1>FORM 3</h1>
+	<form action="process-form-3" method="POST">
+		First Name: <input type="text" name="firstName" placeholder="What's your first name?" />
+		<br>
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
--- src/com/controller/Form1To3Controller.java
+++ src/com/controller/Form1To3Controller.java
@@ -5,21 +5,23 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("forms1-3")
 @Controller
-public class Form1To2Controller {
+public class Form1To3Controller {
 	
 	/////////////////////////////////
 	////// FORM 1
 	
 	@RequestMapping("show-form-1")
 	public String showForm() {
-		return "form-1";
+		return "/form-1";
 	}
 		
 	@RequestMapping("process-form-1")
 	public String processForm() {
-		return "main-menu";
+		return "redirect:/";
 	}
 	
 	//////FORM 1
@@ -33,7 +35,7 @@ public class Form1To2Controller {
 	
 	@RequestMapping("show-form-2")
 	public String showForm2() {		
-		return "form-2";
+		return "/form-2";
 	}
 	
 	@RequestMapping("process-form-2")
@@ -50,10 +52,38 @@ public class Form1To2Controller {
 		// add message to the model
 		model.addAttribute("message", result);
 		
-		return "main-menu";
+		return "redirect:/";
 	}
 	
 	////// FORM 2
 	/////////////////////////////////
 	
+	
+	
+	/////////////////////////////////
+	////// FORM 3
+	
+	@RequestMapping("show-form-3")
+	public String showForm3() {		
+		return "form-3";
+	}
+	
+	@RequestMapping("process-form-3")
+	public String processForm3(
+			@RequestParam("firstName") String firstName,
+			Model model) {
+
+		// convert the data to all caps
+		firstName = firstName.toLowerCase();
+		
+		// create the message
+		String result = "Custom message from form 3 " + firstName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "redirect:/";
+	}
+	////// FORM 2
+	/////////////////////////////////
 }
